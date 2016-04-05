301e39d418f256e5e4050c36f9741e9b3d92602a
Jingkai Tang <jingkaitang@gmail.com>
2016-04-05 15:02:26 +0800

    update: use naive bayes classifier

--- homework2/20newsgroups.py
+++ homework2/20newsgroups.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python
 import os
 import functools
+import chardet
 
+from sklearn.metrics import classification_report, accuracy_score
 from sklearn.cross_validation import train_test_split
 
 DATA_BASE_DIR = 'data'
@@ -22,9 +24,10 @@ if not os.path.exists(data_dir):
 
 
 def prepare_file(folder, filename):
-    filepath = os.path.join(folder, filename)
-    with open(filepath, 'r', encoding='ascii') as f:
-        return f.read()
+    fp = os.path.join(folder, filename)
+    raw_data = open(fp, 'rb').read()
+    codec = chardet.detect(raw_data)
+    return raw_data.decode(codec.get('encoding', 'utf-8'))
 
 
 def get_texts(categories):
@@ -50,6 +53,34 @@ train_docs, test_docs, train_classes, test_classes = train_test_split(documents,
 print('len of train: %d' % len(train_docs))
 print('len of test: %d' % len(test_docs))
 
+#from sklearn.feature_extraction.text import HashingVectorizer
+from nltk.corpus import stopwords
+
+#vectorizer = HashingVectorizer(stop_words='english', non_negative=True, n_features=10000)
+
+from naivebayes import NaiveBayesTextClassifier
+clf = NaiveBayesTextClassifier(categories=categories, min_df=1, lowercase=True, stop_words=stopwords.words('english'))
+clf.train(train_docs, train_classes)
+
+print("-" * 42)
+print("{:<25}: {:>6} articles".format("Total", len(train_docs)))
+print("{:<25}: {:>6} words".format(
+    "Number of words", clf.bag.shape[1]
+))
+print("-" * 42)
+
+print("> Start classify test data")
+predicted_classes = clf.classify(test_docs)
 
 
+def category_to_number(classes, category_type):
+    return list(map(category_type.index, classes))
 
+
+print(classification_report(test_classes, predicted_classes))
+print('-' * 42)
+print("{:<25}: {:>6} articles".format("Test data size", len(test_classes)))
+print("{:<25}: {:>6.2f} %".format(
+    "Accuracy", 100 * accuracy_score(test_classes, predicted_classes))
+)
+print('-' * 42)
